5a3ae0462399d021b973393c30df750261e9faa0
Hanmant <hanmantrachmale@gmail.com>
2024-10-09 08:40:56 +1100

    Add project files.

---
+++ SimpleBookCatalog.Infrastructure/Context/SimpleBookCatalogDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SimpleBookCatalog.Domain.Entities;
+
+namespace SimpleBookCatalog.Infrastructure.Context
+{
+    public class SimpleBookCatalogDbContext:DbContext
+    {
+        public SimpleBookCatalogDbContext(DbContextOptions<SimpleBookCatalogDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Use of Fluent API
+            //modelBuilder.Entity<Book>().Property(e=>e.Title).IsRequired().HasMaxLength(100);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
